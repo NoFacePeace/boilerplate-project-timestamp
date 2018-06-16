40ee094ab526371c411ccca0cde672c1b237c9d6
cht <736090467@qq.com>
2018-06-16 17:36:59 +0800

    fix: add timestamp api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+require('dotenv').config();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,13 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/timestamp/:date_string?', function(req, res) {
+  var date = null;
+  if (req.params.date !== undefined) {
+    var unixTimestamp = parseInt(req,params.date * 1);
+    if (isNaN(unixTimestamp)) {
+      date = new Date(req,params.date);
+    } else {
+      date = new Date(unixTimestamp);
+    }
+  } else {
+    date = new Date(Date.now());
+  }
+  var response = date == "Invalid Date" ? { error: "Invalid Date" } :
+  {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  };
+  res.json(response);
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.use(function(req, res, next) {
+  res.status(404).type('text').send('Not Found');
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
